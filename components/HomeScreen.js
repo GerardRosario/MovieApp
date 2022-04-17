c222607e6c5e25d2acd9c62d8e0cc4387bc1e89c
GerardRosario <55461102+GerardRosario@users.noreply.github.com>
2022-04-16 17:05:59 -0700

    Update HomeScreen.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Button, View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { createStackNavigator, createAppContainer } from '@react-navigation/stack';
  
+//Just a landing page for the app.
+//Also has a decription of the app
 export default class HomeScreen extends Component {
 render() {
     return (
@@ -12,7 +14,7 @@ render() {
           type="clear"
           color="#0d73dd"
           title="Start Searching"
-          onPress={() => this.props.navigation.navigate('Search')} 
+          onPress={() => this.props.navigation.navigate('Search')} //Has a button to naviagte to the search bar
         />
         <Text style={styles.written}>Seenema, is an application containing information called from the OMDB API. It's usage is includes searching for data about movies and tv series.</Text>
         </View>
